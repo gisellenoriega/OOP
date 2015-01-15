2d3dfc0a357021817bcf47e1a319692238218ba8
Giselle Noriega <gnoriega626@gmail.com>
2015-01-15 11:40:11 -0800

    oop

---
+++ inheritance.php
@@ -0,0 +1,33 @@
+<!-- Inheritance is the means by which one or more classes can be derived from a base class -->
+<?php 
+class iPod {
+	public $Name;
+	public $frontcamera;
+	public $doublesidedcameras;
+	public $color;
+	public $generation;
+
+	function __construct($Name, $frontcamera, $doublesidedcamera, $color, $generation) {
+			$this->Name = $Name;
+			$this->frontcamera = $frontcamera;
+			$ths->doublesidedcamera = $doublesidedcamera;
+			$this->color = $color;
+			$this->generation = $generation;
+
+}
+
+	function getName() {
+	return "This is a" . $this->Name . "it only has a" . $this->frontcamera;
+	}
+}
+
+class size extends gigabytes {
+	function __construct($Name, $frontcamera, $doublesidedcamera, $color, $generation)
+			parent::__construct($Name, $frontcamera, $doublesidedcamera, $color, $generation)
+	function hello() {
+	return $this->size;
+	}
+}
+
+$Name = new iPod("iPod4", "white", "small", "16GB");
+print "The iPod is a" . $Name->getName();
